058c08152568447c35c3bd0ff684880ef5c62d18
ah0556702 <ah0556702@otc.edu>
2024-02-26 09:20:00 -0600

    MainActivity updated with Service Listeners, Calculating Logic, Resume and Pause to prevent background running and start up, and SensorEvents that still need work but are constructed

--- app/src/main/java/edu/otc/compass_sensor/MainActivity.java
+++ app/src/main/java/edu/otc/compass_sensor/MainActivity.java
@@ -3,7 +3,12 @@ package edu.otc.compass_sensor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -29,11 +34,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         compassImg = findViewById(R.id.compass); // grab the compass image by ID
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
 
+    @Override
+    protected void onResume() { // register listener to an "active" status when the app is in use
+        super.onResume();
+        sensorManager.registerListener((SensorEventListener) this, magnetometer, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener((SensorEventListener) this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+    }
 
+    @Override
+    protected void onPause() { // pause listeners when the app is not in use
+        super.onPause();
+        sensorManager.unregisterListener((SensorEventListener) this, magnetometer);
+        sensorManager.unregisterListener((SensorEventListener) this, accelerometer);
+    }
 
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == magnetometer) {
+            System.arraycopy(event.values, 0, lastMagnetometer, 0, event.values.length);
+            lastMagnetometerSet = true;
+        } else if (event.sensor == accelerometer) {
+            System.arraycopy(event.values, 0, lastAccelerometer, 0, event.values.length);
+            lastAccelerometerSet = true;
+        }
 
+        if (lastAccelerometerSet && lastMagnetometerSet) {
+            SensorManager.getRotationMatrix(rotationMatrix, null, lastAccelerometer, lastMagnetometer);
+            SensorManager.getOrientation(rotationMatrix, orientation);
 
+            float azimuthInRadians = orientation[0];
+            float azimuthInDegrees = (float) (Math.toDegrees(azimuthInRadians) + 360) % 360;
+
+            Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_compass);
+            Matrix matrix = new Matrix();
+            matrix.postRotate(-azimuthInDegrees);
+            Bitmap rotatedBitmap = Bitmap.createBitmap(originalBitmap, 0, 0, originalBitmap.getWidth(), originalBitmap.getHeight(), matrix, true);
+
+            compassImg.setImageBitmap(rotatedBitmap);
+        }
     }
 }
 
