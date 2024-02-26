519325cc43704bbac8b969f6fceb67f0859d1913
ah0556702 <ah0556702@otc.edu>
2024-02-26 09:11:29 -0600

    MainActivity updated with private var fields for each sensor component and array

--- app/src/main/java/edu/otc/compass_sensor/MainActivity.java
+++ app/src/main/java/edu/otc/compass_sensor/MainActivity.java
@@ -2,16 +2,38 @@ package edu.otc.compass_sensor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     // public static final String STRING_TYPE_MAGNETIC_FIELD = null;
+    private ImageView compassImg;
+    private SensorManager sensorManager;
+    private Sensor magnetometer;
+    private Sensor accelerometer;
+    private float[] lastAccelerometer = new float[3];
+    private float[] lastMagnetometer = new float[3];
+    private boolean lastAccelerometerSet = false;
+    private boolean lastMagnetometerSet = false;
+    private float[] rotationMatrix = new float[9];
+    private float[] orientation = new float[3];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        compassImg = findViewById(R.id.compass); // grab the compass image by ID
+
+
+
+
+
     }
 }
 
